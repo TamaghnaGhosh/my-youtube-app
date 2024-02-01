69b2fbef8de57a26215e37895bd68500d71be1a8
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-02-01 17:43:19 +0530

    all tests are added

--- src/__tests__/Name.test.jsx
+++ src/__tests__/Name.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Name from '../../Name';
+import Name from '../Name';
 import { expect, it} from 'vitest';
 
 it('renders Name element', () => {
