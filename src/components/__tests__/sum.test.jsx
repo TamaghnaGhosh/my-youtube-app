58b5f1c7c35083d0d21c2d27af9a19d50b34ff4f
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-02-01 17:29:20 +0530

    __tests__

--- src/components/__tests__/sum.test.jsx
+++ src/components/__tests__/sum.test.jsx
@@ -1,6 +1,6 @@
 // sum.test.js
 import { expect, it } from "vitest";
-import { sum } from "./sum";
+import { sum } from "../../sum";
 
 it("adds 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4);
