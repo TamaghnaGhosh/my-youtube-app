f232fdaa2fb1186dcd3e02db259ebf2fdab5e7e9
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-02-04 16:30:21 +0530

    middleware: (getDefaultMiddleware) =>     getDefaultMiddleware({       serializableCheck: false,     }),     middleware false

--- src/utilts/store.js
+++ src/utilts/store.js
@@ -4,6 +4,10 @@ import persistedReducer from "./rootReducer";
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
