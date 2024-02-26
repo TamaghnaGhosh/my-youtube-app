43b49cd80ebc6ec7d5d85eccfac71ac2b280e757
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-02-26 15:12:24 +0530

    chaged

--- src/components/Body/WatchPage.jsx
+++ src/components/Body/WatchPage.jsx
@@ -70,8 +70,8 @@ const WatchPage = () => {
             <LiveChat />
           ) : (
             <>
-              <div className="m-0 p-0 font-bold text-xl">Recommendation</div>
-              <span>Still a work in progress.....</span>
+              <div className="m-2 p-2 font-bold text-xl">Recommendation</div>
+              <span className="m-2 p-2">Still a work in progress.....</span>
             </>
           )}
         </div>
